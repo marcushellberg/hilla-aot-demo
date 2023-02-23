ba0bffe6a2df103f18e7ab77773b97be736dbd25
Marcus Hellberg <marcus@vaadin.com>
2023-02-23 07:08:23 -0800

    Update readme

--- native.sh
+++ native.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./mvnw -Pproduction -Pnative spring-javaformat:apply clean package native:compile && ./target/flow-native
+./mvnw -Pproduction -Pnative spring-javaformat:apply clean package native:compile && ./target/hilla-aot-demo
--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -2,9 +2,11 @@ package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+import org.springframework.aot.hint.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 /**
  * The entry point of the Spring Boot application.
@@ -15,7 +17,21 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 @SpringBootApplication
 @Theme(value = "hilla-aot-demo")
+@ImportRuntimeHints(Application.Hints.class)
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+	static class Hints implements RuntimeHintsRegistrar {
+
+		@Override
+		public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+			var reflections = hints.reflection();
+			var memberCategories = MemberCategory.values();
+			reflections.registerType(
+					TypeReference.of("dev.hilla.push.PushConfigurer.EmbeddedAtmosphereInitializer.class"),
+					memberCategories);
+		}
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
